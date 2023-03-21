261282a7f69146b7f91285802be9410f71230170
Alexandre Marques <alexandre.fnz.oliveira@hotmail.com>
2023-03-21 13:18:26 -0300

    ...

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: "./login.component.html",
+  selector: "app-login",
+  styleUrls: ["./login.component.css"]
+})
+export class LoginComponent{
+
+}
