25216d4651f73b37a3f3feb35dcb0dad1b73deb2
Alexandre Marques <alexandre.fnz.oliveira@hotmail.com>
2023-03-21 14:44:55 -0300

    Implementação de validações no form

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../modelo/usuario";
 
 @Component({
   templateUrl: "./login.component.html",
@@ -6,5 +7,9 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent{
+  public usuario;
 
+  constructor(){
+    this.usuario = new Usuario();
+  }
 }
